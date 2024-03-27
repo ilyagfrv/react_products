9f7081a59e4b4115a111fd754fc37c6d9e20529d
ilyagfrv <ilyagafarov@bk.ru>
2024-03-27 17:35:18 +0600

    basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,79 @@
 import './App.css'
+import image from './assets/icons/icon.svg'
 
 export default function App() {
   return (
-    <div>
-      <h1>Product App</h1>
+    <div className='app'>
+      <header className='header'>
+        <h1>Grocery store</h1>
+        <a href='#'>Cart</a>
+      </header>
+
+      <main className='main'>
+        <aside className='sidebar'>
+          <nav>
+            <ul>
+              <li>dairy</li>
+              <li>dairy</li>
+              <li>dairy</li>
+              <li>dairy</li>
+            </ul>
+          </nav>
+        </aside>
+
+        <section className='content'>
+          <ul className='products'>
+            <li className='product__item'>
+              <img src={image} alt='' />
+              <div>
+                <h4>name</h4>
+                <h4>1000 rub</h4>
+              </div>
+              <button>add to cart</button>
+            </li>
+            <li className='product__item'>
+              <img src={image} alt='' />
+              <div>
+                <h4>name</h4>
+                <h4>1000 rub</h4>
+              </div>
+              <button>add to cart</button>
+            </li>
+            <li className='product__item'>
+              <img src={image} alt='' />
+              <div>
+                <h4>name</h4>
+                <h4>1000 rub</h4>
+              </div>
+              <button>add to cart</button>
+            </li>
+            <li className='product__item'>
+              <img src={image} alt='' />
+              <div>
+                <h4>name</h4>
+                <h4>1000 rub</h4>
+              </div>
+              <button>add to cart</button>
+            </li>
+            <li className='product__item'>
+              <img src={image} alt='' />
+              <div>
+                <h4>name</h4>
+                <h4>1000 rub</h4>
+              </div>
+              <button>add to cart</button>
+            </li>
+            <li className='product__item'>
+              <img src={image} alt='' />
+              <div>
+                <h4>name</h4>
+                <h4>1000 rub</h4>
+              </div>
+              <button>add to cart</button>
+            </li>
+          </ul>
+        </section>
+      </main>
     </div>
   )
 }
