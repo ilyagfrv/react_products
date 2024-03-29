b705a1117605f032bf91a91e479c3af4b12e1507
ilyagfrv <ilyagafarov@bk.ru>
2024-03-29 18:00:21 +0600

    types : product

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  id: number
+  name: string
+  price: number
+  image: string
+  brand: string
+  weight: number
+  category: string
+  quantity: number
+  country: string
+}
