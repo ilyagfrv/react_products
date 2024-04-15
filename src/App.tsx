9c485da48c8d755482bf5d740620159f256c6ead
ilyagfrv <ilyagafarov@bk.ru>
2024-04-15 16:34:31 +0600

    main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
-import { Header } from 'components'
+import MainLayout from 'layouts/MainLayout'
 import { Main, Details, Favorite, Cart, NotFound } from 'pages'
 
 export default function App() {
   return (
     <div className='app'>
-      <Header />
-
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/products/:id' element={<Details />} />
-        <Route path='/favorite' element={<Favorite />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<Main />} />
+          <Route path='products/:id' element={<Details />} />
+          <Route path='favorite' element={<Favorite />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Routes>
     </div>
   )
